1c6b10adbffcd8728d3934ec0152d8e17f2f6139
Dowling1dow <Dowling12dow@hotmail.com>
2016-09-29 18:44:13 +0100

    First commit, ReadMe explains what the script is

---
+++ is_my_train_late/README.md
@@ -0,0 +1,6 @@
+This is just a small script I created that will text me in the morning before I'm expected to leave my house and tell me if my train is going to be more than 5 minutes late.
+Irish Rail have a habit of being late and I hated turning up to the station and having to stand on the platform for 15 minutes past the scheduled time of arrival.
+
+The script runs on a server at a scheduled time 5 days during the week. 
+
+
---
+++ is_my_train_late/is_my_train_late.py
@@ -0,0 +1,45 @@
+import requests, getpass
+from lxml import html
+from twilio.rest import TwilioRestClient
+
+def get_late_trains():
+	irish_rail_url = 'http://www.irishrail.ie/timetables/live-departure-times'
+	location_data = dict(key="{{ STATION LOCATION }}")
+	depart_page = requests.post(irish_rail_url, data=location_data)
+	root = html.fromstring(depart_page.content)
+
+	a_tags = root.xpath('..//a[contains(text(), "Dublin Heuston")]')
+	late_trains = []
+
+	for a_tag in a_tags:
+	 	tr_tag = a_tag.getparent().getparent()
+		sch = int(tr_tag[2].text.replace(":", ""))
+		eta = int(tr_tag[3].text.replace(":", ""))
+		sch_str = str(sch)
+		time_delayed = abs(sch - eta)
+		if (time_delayed > 5):
+			late_trains.append(sch_str[:2]+":"+sch_str[2:] + " train delayed by "+ str(time_delayed))
+
+	return late_trains
+
+def send_text(late_trains):
+	# Using Twilio here
+	account_sid = "{{ account_sid }}"
+	auth_token  = "{{ auth_token }}"
+	client = TwilioRestClient(account_sid, auth_token)
+	message = client.messages.create(
+		body=compose_text(late_trains),
+	    to="+{{ ENTER NUMBER }}",    # My phone number
+	    from_="+{{ ENTER TWILIO NUMBER }}") # Twilio number
+
+def compose_text(late_train_list):
+	msg = ""
+	for train in late_train_list:
+		msg = msg + train + "\n"
+	return msg	
+
+
+late_trains = get_late_trains()
+if len(late_trains) != 0:
+	send_text(late_trains)
+
